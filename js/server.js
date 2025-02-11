d0cd2f3ba3f9ec2626f2c9cb331d6aadc81538af
cclouud <alexgarciap3108@gmail.com>
2025-02-11 21:13:43 +0100

    cambio de forma mapa

--- js/server.js
+++ js/server.js
@@ -1,11 +1,7 @@
+    document.addEventListener("DOMContentLoaded", function () {
+        var map = L.map('map').setView([37.1773, -3.5986], 13); // Centrado en Granada
 
-var map = L.map('map').setView([37.1773, -3.5986], 13); // [Latitud, Longitud], Zoom
-
-        // Cargar los tiles de OpenStreetMap
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; OpenStreetMap contributors'
         }).addTo(map);
-
-        // Agregar un marcador en Granada
-        var marker = L.marker([37.1773, -3.5986]).addTo(map)
-            .bindPopup("<b>Granada, España</b><br>Una ciudad hermosa.").openPopup();
+    });
